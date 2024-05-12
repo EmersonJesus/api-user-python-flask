e97b64d7286cbc0545c3c643e2b5ce935e5bdccc
Emerson Jesus <Emersoncruz73@hotmail.com>
2024-05-12 15:19:00 -0300

    create update user route

--- app/views/users.py
+++ app/views/users.py
@@ -23,3 +23,26 @@ def post_user():
         return jsonify({'message': 'User successfully registered', 'data': result}), 201
     except Exception as e:
         return jsonify({'message': 'Unable to create user', 'error': str(e)}), 500
+
+def update_user(id):
+    username = request.json.get('username')
+    password = request.json.get('password')
+    name = request.json.get('name')
+    email = request.json.get('email')
+
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'message': "User doesn't exist", 'data': {}}), 404
+
+    pass_hash = generate_password_hash(password)
+
+    try:
+        user.username = username
+        user.password = pass_hash
+        user.name = name
+        user.email = email
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'message': 'User successfully updated', 'data': result}), 200
+    except Exception as e:
+        return jsonify({'message': 'Unable to update user', 'error': str(e)}), 500
